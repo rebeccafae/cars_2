263c6ef8ab13c9cbdf60013a712d07e97f0d7644
Anthony Falzetti <anthony.falzetti@gmail.com>
2015-09-30 13:44:30 -0600

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'cars#index'
+  resources :cars
+  resources :users,
+                    only: [:new, :create],
+                    path_names: { new: "signup"}
+
+  get 'login' => 'sessions#login'
+  post 'login' => 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+end
---
+++ spec/features/user_authentication_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+feature "User Authentication" do
+  scenario "allows a user to signup" do 
+    visit '/'
+
+    expect(page).to have_link('Signup')
+
+    click_link 'Signup'
+
+    fill_in 'First name', with: "bob"
+    fill_in 'Last name', with: 'smith'
+    fill_in 'Email', with: 'bob@smith.com'
+    fill_in 'Password', with: 'sup3rs3krit'
+    fill_in 'Password confirmation', with: 'sup3rs3krit'
+
+    click_button 'Create User'
+
+    expect(page).to have_text('Thank you for signing up Bob')
+    expect(page).to have_text('Signed in as bob@smith.com')
+  end
+
+  scenario "allows existing users to login" do
+    user = FactoryGirl.create(:user)
+
+    visit '/'
+
+    expect(page).to have_link("Login")
+
+    click_link('Login')
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Login"
+    expect(page).to have_text("Welcome back #{user.first_name.titleize}")
+    expect(page).to have_text("Signed in as #{user.email}")
+
+  end
+  
+  scenario "does not allow existing users to login with an invalid password" do
+    user = FactoryGirl.create(:user, password: 'sup3rs3krit')
+
+    visit '/'
+
+    expect(page).to have_link('Login')
+
+    click_link('Login')
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'Bad_Password'
+
+    click_button 'Login'
+    expect(page).to have_text('Invalid email or password')
+   end
+
+   scenario 'allows a logged in user to logout' do
+     user = FactoryGirl.create(:user)
+
+    visit '/'
+
+    expect(page).to have_link("Login")
+
+    click_link('Login')
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Login"
+    expect(page).to have_text("Signed in as #{user.email}")
+
+    expect(page).to have_link('Logout')
+
+    click_link('Logout')
+
+    expect(page).to have_text("#{user.email} has been logged out.")
+    expect(page).to_not have_text("Welcome back #{user.first_name.titleize}")
+    expect(page).to_not have_text("Signed in as #{user.email}")
+    end
+end  
